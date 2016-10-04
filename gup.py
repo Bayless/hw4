2ab6697aa7800a71c5423e59529879f6cf4dde95
Bayless <bsmithsalzberg@stuy.edu>
2016-10-04 19:41:29 -0400

    added utils

--- gup.py
+++ gup.py
@@ -4,9 +4,25 @@
 #2016-09-29
 
 from flask import Flask, render_template, request
+from hashlib import sha1
 
 app = Flask(__name__)
 
+
+@app.route("/login/")
+def home():
+        processForms.loadDict()
+        return render_template("dn.html")
+
+#WITHOUT hash rn
+@app.route("/authenticate/", methods=['POST'])
+def auth():
+        retAuth = processForms.authenticate(request.form['user'], request.form['pass'])
+        return render_template("auth.html", verdict=retAuth[0], reason=retAuth[1])
+
+@app.route("/register/", methods=['POST'])
+def reg():
+        return render_template("dn.html", errorMsg=processForms.register(request.form['newUser'],request.form['newPass']))
 @app.route("/")
 def temp():
     print"\n\n\n"
@@ -17,16 +33,18 @@ def temp():
     return render_template("input.html")
 
 @app.route("/auth", methods=['POST'])
+def checkU(username):
+    if file.read().find(username) != -1:
+        return True
+    
 def author():
     print"\n\n\n"
     print "this the apperrino"
     print app
     print "this is username"
-    if request.form['username'] == #username in csv
-        "bayle" and request.form['password'] ==#password in csv
-        "bss":
+    if request.form['username'] == "bayle" and request.form['password'] == "bss":#username in csv, password in csv
         return render_template("success.html")
-    else if:
+    else:
         return render_template("failure.html")
 
 if __name__ == "__main__":
