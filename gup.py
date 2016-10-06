b13760a03e33699641fc8a82b65c1ffaa8509dc6
Bayless <bsmithsalzberg@stuy.edu>
2016-10-06 13:05:42 -0400

    classwork --- fixfixfix

--- gup.py
+++ gup.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python
-
-#Bayle SMith-Salzberg
+#Bayle Smith-Salzberg
 #SoftDev1 pd8
 #HW04 -- Big, Heavy, Wood
 #2016-09-29
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,url_for,session
 from hashlib import sha1
 from utils import verify
+
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/login/")
 def logRegister():
-        verify.makeCsv()
-        return render_template("input.html")
+        if 'username' in session:
+            return 'logged in as ' + session['username']
+        return 'you are not logged in'
 
-@app.route("/authenticate/", methods=['POST'])
+@app.route("/login/",methods=['POST'])
 def auth():
         retAuth = verify.authenticate(request.form['username'], request.form['password'])
         if retAuth:
-                return render_template("success.html")
+                session['username']=request.form['username']
+                return redirect(url_for('logRegister'))
         else:
-                return render_template("failure.html")
+                return 'wrong password, try again'
+
 @app.route("/register/", methods=['POST'])
 def reg():
-        return render_template("input.html", errorMsg=verify.register(request.form['usr'],request.form['pwd']))
+        return redirect(url_for('logRegister'))
+
+@app.route("/jacobo")
+def js():
+    return redirect("http://xkcd.com")
 
 if __name__ == "__main__":
     app.debug = True
